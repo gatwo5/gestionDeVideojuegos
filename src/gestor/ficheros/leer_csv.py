aa56a6eb2ad28543ec42b5232a5d70809b02c390
Riz-baig <baigrizmirza@gmial.com>
2026-01-19 21:04:16 +0100

    feat: leer catalogo desde CSV

--- src/gestor/ficheros/leer_csv.py
+++ src/gestor/ficheros/leer_csv.py
@@ -1,1 +1,25 @@
+import csv
 from src.gestor.catalogo import catalogo
+from os import strerror
+
+
+def leer_catalogo_csv(nombre_fichero):
+    try:
+        with open(f"src/gestor/ficheros/escribir_ficheros/{nombre_fichero}.csv", "r") as fichero:
+            reader = csv.DictReader(fichero, delimiter="|")
+
+            catalogo.clear()  # vaciamos el catálogo antes de cargar
+
+            for fila in reader:
+                catalogo.append({
+                    "titulo": fila["titulo"],
+                    "anio": int(fila["anio"]),
+                    "genero": fila["genero"]
+                })
+
+            print("Archivo leído correctamente")
+
+    except IOError as e:
+        print("Error durante la operacion de archivos:", strerror(e.errno))
+        exit(e.errno)
+
